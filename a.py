52f8b5fa0d46614c52e9e3ea420a7718d9ac3acc
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-17 19:17:33 +0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,252 @@
+import os,sys,random,time
+try:
+    import names
+except:
+    os.system("pip install names")
+    pass
+ooo=os.name
+if ooo=="posix":
+    logo='''
+            \n
+             ▄████▄   ▒█████   ███▄ ▄███▓ ▄▄▄▄    ▒█████  
+            ▒██▀ ▀█  ▒██▒  ██▒▓██▒▀█▀ ██▒▓█████▄ ▒██▒  ██▒
+            ▒▓█    ▄ ▒██░  ██▒▓██    ▓██░▒██▒ ▄██▒██░  ██▒
+            ▒▓▓▄ ▄██▒▒██   ██░▒██    ▒██ ▒██░█▀  ▒██   ██░
+            ▒ ▓███▀ ░░ ████▓▒░▒██▒   ░██▒░▓█  ▀█▓░ ████▓▒░
+            ░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░   ░  ░░▒▓███▀▒░ ▒░▒░▒░ 
+              ░  ▒     ░ ▒ ▒░ ░  ░      ░▒░▒   ░   ░ ▒ ▒░ 
+            ░        ░ ░ ░ ▒  ░      ░    ░    ░ ░ ░ ░ ▒  
+            ░ ░          ░ ░         ░    ░          ░ ░  
+            ░                                  ░          
+         ================================================
+                - = [ Created By Zed Coder ] = -
+         ================================================
+            [ 1 ] - Combo Number Iraq (v1)
+            [ 2 ] - Combo Number Iraq (v2)
+            [ 3 ] - Combo User (v1)
+            [ 4 ] - Combo User (v2)
+            [ 5 ] - Combo User (auto-v1)
+            [ 6 ] - Combo User (auto-v2)
+            [ 7 ] - Combo @Gmail.com (v1)
+            [ 8 ] - Combo @Gmail.com (v2)
+            [ 9 ] - Combo @Hotmail.com (v1)
+            [ 10 ] - Combo @Hotmail.com (v2)
+            [ 11 ] - Combo @Yahoo.com (v1)
+            [ 12 ] - Combo @Yahoo.com (v2)
+            [ 13 ] - Contact (!)
+            [ 0 ] - Exit () l  File Saved : /sdcard/..
+        =============================================='''
+    def c1():
+        for i in range(999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(num-v1).txt", "a")
+            print("+964770"+str(ran)+":"+str(ran))
+            print("+964771"+str(ran)+":"+str(ran))
+            print("+964772"+str(ran)+":"+str(ran))
+            print("+964773"+str(ran)+":"+str(ran))
+            print("+964774"+str(ran)+":"+str(ran))
+            print("+964750"+str(ran)+":"+str(ran))
+            print("+964751"+str(ran)+":"+str(ran))
+            print("+964752"+str(ran)+":"+str(ran))
+            print("+964753"+str(ran)+":"+str(ran))
+            print("+964754"+str(ran)+":"+str(ran))
+            print("+964780"+str(ran)+":"+str(ran))
+            print("+964781"+str(ran)+":"+str(ran))
+            print("+964782"+str(ran)+":"+str(ran))
+            print("+964783"+str(ran)+":"+str(ran))
+            print("+964784"+str(ran)+":"+str(ran))
+            sys.stdout.flush()
+        c10()
+    def c2():
+        for i in range(999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(num-v2).txt", "a")
+            print("+964770"+str(ran)+":0770"+str(ran))
+            print("+964771"+str(ran)+":0771"+str(ran))
+            print("+964772"+str(ran)+":0772"+str(ran))
+            print("+964773"+str(ran)+":0773"+str(ran))
+            print("+964774"+str(ran)+":0774"+str(ran))
+            print("+964750"+str(ran)+":0750"+str(ran))
+            print("+964751"+str(ran)+":0751"+str(ran))
+            print("+964752"+str(ran)+":0752"+str(ran))
+            print("+964753"+str(ran)+":0753"+str(ran))
+            print("+964754"+str(ran)+":0754"+str(ran))
+            print("+964780"+str(ran)+":0780"+str(ran))
+            print("+964781"+str(ran)+":0781"+str(ran))
+            print("+964782"+str(ran)+":0782"+str(ran))
+            print("+964783"+str(ran)+":0783"+str(ran))
+            print("+964784"+str(ran)+":0784"+str(ran))
+            sys.stdout.flush()
+        c10()
+    def c3():
+        user=input("         user:")
+        for i in range(99999):
+            ran=random.randint(10000000, 99999999)
+            sys.stdout=open("Combo(user-v1).txt", "a")
+            print(str(user)+":"+str(ran))
+            sys.stdout.flush()
+        c10()
+    def c4():
+        user=input("         user:")
+        user1=input("         user in pass: ")
+        for i in range(99999):
+            ran=random.randint(10000000, 99999999)
+            sys.stdout=open("Combo(user-v2).txt", "a")
+            print(str(user)+":"+str(user1)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c5():
+        for i in range(99999):
+            user=names.get_first_name()
+            sys.stdout=open("Combo(user-auto-v1).txt", "a")
+            print(str(user)+str(i)+":"+str(user)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c6():
+        user1=names.get_first_name()
+        user=names.get_first_name()
+        for i in range(99999):
+            user1=names.get_first_name()
+            user=names.get_first_name()
+            sys.stdout=open("Combo(user-auto-v2).txt", "a")
+            print(str(user)+"_"+str(user1)+":"+str(user)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c7():
+        user1=input("         (gmail) 1 Name: ")
+        user2=input("         () newani: ")
+        user3=input("         (gmail) 2 Name: ")
+        for i in range(99999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(gmail-v1).txt", "a")
+            print(str(user1+str(user2)+str(user3)+str(i))+"@gmail.com:"+str(user1)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c77():
+        user1=input("         (gmail) 1 Name: ")
+        user2=input("         () newani: ")
+        user3=input("         (gmail) 2 Name: ")
+        for i in range(99999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(gmail-v2).txt", "a")
+            print(str(user1+str(user2)+str(user3)+str(i))+"@gmail.com:"+str(user1)+str(user2)+str(user3)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c8():
+        user1=input("         (hotmail) 1 Name: ")
+        user2=input("         () newani: ")
+        user3=input("         (hotmail) 2 Name: ")
+        for i in range(99999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(hotmail-v1).txt", "a")
+            print(str(user1+str(user2)+str(user3)+str(i))+"@hotmail.com:"+str(user1)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c88():
+        user1=input("         (hotmail) 1 Name: ")
+        user2=input("         () newani: ")
+        user3=input("         (hotmail) 2 Name: ")
+        for i in range(99999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(hotmail-v2).txt", "a")
+            print(str(user1+str(user2)+str(user3)+str(i))+"@hotmail.com:"+str(user1)+str(user2)+str(user3)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c9():
+        user1=input("         (yahoo) 1 Name: ")
+        user2=input("         () newani: ")
+        user3=input("         (yahoo) 2 Name: ")
+        for i in range(99999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(yahoo-v1).txt", "a")
+            print(str(user1+str(user2)+str(user3)+str(i))+"@yahoo.com:"+str(user1)+str(i))
+            sys.stdout.flush()
+    def c99():
+        user1=input("         (yahoo) 1 Name: ")
+        user2=input("         () newani: ")
+        user3=input("         (yahoo) 2 Name: ")
+        for i in range(99999):
+            ran=random.randint(1000000, 9999999)
+            sys.stdout=open("Combo(yahoo-v2).txt", "a")
+            print(str(user1+str(user2)+str(user3)+str(i))+"@yahoo.com:"+str(user1)+str(user2)+str(user3)+str(i))
+            sys.stdout.flush()
+        c10()
+    def c10():
+        os.system("clear")
+        print(logo)
+        coco=input("  ? :  ")
+        if coco=="1":
+            c1()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="2":
+            c2()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="3":
+            c3()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="4":
+            c4()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="5":
+            c5()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="6":
+            c6()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="7":
+            c7()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="8":
+            c77()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="9":
+            c8()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="10":
+            c88()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="11":
+            c9()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="12":
+            c99()
+            print("tawaw")
+            time.sleep(3)
+            os.system("exit")
+        elif coco=="13":
+            os.system("xdg-open https://t.me/zed_cracker_1")
+            time.sleep(1)
+            c10()
+        elif coco=="0":
+            print("Ok")
+            time.sleep(4)
+            os.system("exit")
+        else:
+            time.sleep(1)
+            c10()
+    time.sleep(1)
+    c10()
+else:
+    os.sys.exit()
